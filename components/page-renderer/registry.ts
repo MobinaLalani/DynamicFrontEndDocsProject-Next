5055ecd37f1941c1c801e6acc10baa68bb3a55eb
MobinaLalani <MobinaLalani529@gmail.com>
2026-07-18 10:05:36 +0330

    change ui components file location

--- components/page-renderer/registry.ts
+++ components/page-renderer/registry.ts
@@ -1,10 +1,10 @@
-import { CodeBlock } from "@/components/page-renderer/blocks/CodeBlock";
-import { EndpointBlock } from "@/components/page-renderer/blocks/EndpointBlock";
-import { FieldGroupBlock } from "@/components/page-renderer/blocks/FieldGroupBlock";
-import { HeadingBlock } from "@/components/page-renderer/blocks/HeadingBlock";
-import { NoteBlock } from "@/components/page-renderer/blocks/NoteBlock";
-import { ParagraphBlock } from "@/components/page-renderer/blocks/ParagraphBlock";
-import { TableBlock } from "@/components/page-renderer/blocks/TableBlock";
+import { CodeBlock } from "@/components/ui/pageBuilderComponents/CodeBlock";
+import { EndpointBlock } from "@/components/ui/pageBuilderComponents/EndpointBlock";
+import { FieldGroupBlock } from "@/components/ui/pageBuilderComponents/FieldGroupBlock";
+import { HeadingBlock } from "@/components/ui/pageBuilderComponents/HeadingBlock";
+import { NoteBlock } from "@/components/ui/pageBuilderComponents/NoteBlock";
+import { ParagraphBlock } from "@/components/ui/pageBuilderComponents/ParagraphBlock";
+import { TableBlock } from "@/components/ui/pageBuilderComponents/TableBlock";
 import type { PageBlockRegistry } from "@/components/page-renderer/types";
 
 export const pageBlockRegistry: PageBlockRegistry = {
